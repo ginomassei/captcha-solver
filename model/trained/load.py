9191b49d6630bceeda9c0d635dbbdf7b74c49cc3
ginomassei <ginomassei@hotmail.com>
2021-05-29 18:59:09 -0300

    Added all the loaders for the tained model.

--- model/trained/load.py
+++ model/trained/load.py
@@ -1,21 +1,34 @@
 import torch
-from nnet import NeuralNet
-from labels import labels
-import functions
-from skimage import io
 from torchvision import transforms
+from PIL import Image
+from model.src.models.Network import NeuralNetwork
+from inversed_labels import labels
 
 
 if __name__ == '__main__':
-    model = NeuralNet(36)
-    model.load_state_dict(torch.load('captcha-recognizer.pth'))
+    # Instanciate the model.
+    model = NeuralNetwork()
+
+    # Load the state dict of the model.
+    model_path = 'trainedmodel.pth'
+    model.load_state_dict(torch.load(model_path))
+
+    # Set the model into evaluation state.
     model.eval()
 
-    
-    image = io.imread('1-82480.jpeg', as_gray=True)
-    
-    image = torch.tensor(image)
-    print(image.shape)
-    
-    exit()
-    result = model.forward(image)
+    # Load an image.
+    image_path = '../data/digits/n-1123.jpeg'
+    X = Image.open(image_path).convert('L')
+
+    # Transform the ndarray into a tensor.
+    X = transforms.ToTensor()(X)
+    print(X.shape)
+    # Remove the first dimention.
+    X = X.unsqueeze(0)
+    print(X.shape)
+    # Pass the image to the model.
+    predictions = model(X)
+
+    predictions = torch.nn.Softmax(dim=1)(predictions)
+    predicted_value = predictions.argmax(1).item()
+    print(labels[predicted_value])
